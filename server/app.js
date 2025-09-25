c2142c550b16135f75d602a943895d0a416b5a9d
Mj <markjoseph.aspe@gmail.com>
2025-09-25 18:42:39 +0800

    Server: Setting up app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,23 @@
 import "./configs/dotenv.config.js";
 import express from "express";
+import expressSetupMiddleware from "./configs/index.config.js";
+import {
+  notFoundHandler,
+  globalErrorHandler,
+} from "./middlewares/errorHandler.middleware.js";
 
 const app = express();
+
+// Setup all middleware configurations
+expressSetupMiddleware(app);
+
+// Mounting all Routes
+// app.use("/api", allRoutes); // We'll uncomment this when we have routes
+
+// Handle 404 errors
+app.use(notFoundHandler);
+
+// Global error handler (must be last)
+app.use(globalErrorHandler);
 
 export default app;
