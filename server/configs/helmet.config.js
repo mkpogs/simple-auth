c79f4fc193ab719d0afde5cfbfc6b45e5b720672
Mj <markjoseph.aspe@gmail.com>
2025-09-25 15:28:44 +0800

    Server: express custom configs - Added helmet config

---
+++ server/configs/helmet.config.js
@@ -0,0 +1,18 @@
+import helmet from "helmet";
+
+export const helmetConfig = helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'"],
+      imgSrc: ["'self'", "data:", "https:"],
+      connectSrc: ["'self'"],
+      fontSrc: ["'self'"],
+      objectSrc: ["'none'"],
+      mediaSrc: ["'self'"],
+      frameSrc: ["'none'"],
+    },
+  },
+  crossOriginEmbedderPolicy: false,
+});
