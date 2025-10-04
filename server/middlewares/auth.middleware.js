df2847c9c9edacfe1d9e25cca00baf4a24920f5f
Mj <markjoseph.aspe@gmail.com>
2025-10-04 14:40:30 +0800

    Server-Middlewares: auth middleware

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.model.js";
+import AppError from "../utils/AppError.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    // Get token from header
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return next(
+        new AppError("You are not logged in! Please log in to access", 401)
+      );
+    }
+
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+    // Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(new AppError("User no longer exists.", 401));
+    }
+
+    // Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next(
+        new AppError("assword changed recently. Please log in again.", 401)
+      );
+    }
+
+    // Grant access
+    req.user = currentUser;
+    next();
+  } catch (error) {
+    return next(new AppError("Invalid token. Please log in again.", 401));
+  }
+};
