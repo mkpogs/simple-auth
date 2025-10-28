4c05fc2c590fe7e63fcbdc459ce246ad077851df
Mj <markjoseph.aspe@gmail.com>
2025-10-28 14:28:02 +0800

    Client-services: userService - AVATAR MANAGEMENT (uploadAvatar, deleteAvatar)

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,4 +1,3 @@
-import { updateProfile } from "../../../server/controllers/user.controller";
 import api from "../utils/api";
 
 /**
@@ -28,6 +27,37 @@ export const userService = {
    */
   updateProfile: async (profileData) => {
     const response = await api.put("/users/profile", profileData);
+    return response.data;
+  },
+
+  // ===== AVATAR MANAGEMENT =====
+
+  /**
+   * POST /api/users/avatar
+   */
+  uploadAvatar: async (file, onProgress) => {
+    const formData = new FormData();
+    formData.append("avatar", file);
+
+    const response = await api.post("/users/avatar", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress: (ProgressEvent) => {
+        const progress = Math.round(
+          (ProgressEvent.loaded * 100) / ProgressEvent.total
+        );
+        onProgress?.(progress);
+      },
+    });
+    return response.data;
+  },
+
+  /**
+   * DELETE /api/users/avatar
+   */
+  deleteAvatar: async () => {
+    const response = await api.delete("/users/avatar");
     return response.data;
   },
 };
